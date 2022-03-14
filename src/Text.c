8496610c710f7047d16b461f894e6b78f759ae0e
root <root@localhost>
2022-03-14 13:43:05 +0600

    auto publish 1647243785137

--- src/Text.c
+++ src/Text.c
@@ -1,10 +1,9 @@
-
 #include<stdio.h>
 #include<conio.h>
 #include<graphics.h>
 void main(){
 	int gd = DETECT ,gm;
 initgraph(&gd,&gm,"c:\\turboc3\\bgi");
-	outtext("VISHAL");
+	outtext("Asif");
 getch();
 }
