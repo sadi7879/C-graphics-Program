70c8b0ad0433e7c38f67afd89883b7bf7861dc9b
root <root@localhost>
2022-03-14 13:44:49 +0600

    auto publish 1647243889522

--- src/line.c
+++ src/line.c
@@ -3,10 +3,24 @@
 #include<graphics.h>
 void main(){
 	int gd = DETECT ,gm;
-int x1=10 , x2= 500;
-	int y1=200 , y2 =200;
-	int radius =100;
-	initgraph(&gd,&gm,"c:\\turboc3\\bgi");
-	line(x1,y1,x2,y2);
+initgraph(&gd,&gm,"c:\\turboc3\\bgi");
+	//ground
+	line(10,300,500,300);
+	line(10,302,500,302);
+	//home
+	rectangle(50,150,200,298);
+	//door
+	rectangle(55,185,110,295);
+	//windows
+	rectangle(140,200,180,240);
+	line(140,220,180,220);
+	line(160,200,160,240);
+	//roof
+	line(30,150,220,150);
+	line(115,50,220,150);
+	line(115,50,28,150);
+	//text
+	settextstyle(1,HORIZ_DIR,5);
+	outtextxy(300,200,"MY HOME");
 getch();
 }
